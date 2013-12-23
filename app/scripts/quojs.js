68b16108576b49a5ae8e1b8fcee5d2dfcef0e6ef
akh2103 <akh2103@gmail.com>
2013-12-22 22:35:09 -0600

    on change event is working

--- app/scripts/quojs.js
+++ app/scripts/quojs.js
@@ -3,14 +3,22 @@ $$('#test').tap(function() {
     alert("works");
 });
 
-$$('#concrete_quantity').tap(function() {
+$$(document).on('change', '#concreteForm', function() {
     var depth=$$('#concrete_depth').val();
     console.log(depth);
     var width=$$('#concrete_width').val();
     console.log(width);
     var height=$$('#concrete_length').val();
     console.log(height);
-    var answer = getSquareFeet(depth, width, height);
-    
+    var squareFeet = getSquareFeet(depth, width, height);
+    var quantity =$$('#concrete_quantity').val();
+    var answer = squareFeet * quantity;
     $$('#showcalculationconcrete').text(answer);
+});
+
+$$(document).on('change', '#equipment', function() {
+    var equipQuantity =$$('#equipment_quantity_1').val();
+    var equipRate =$$('#equipment_rate_1').val();
+    var answer = equipQuantity * equipRate;
+    $$('#showcalculationequipment').text(answer);
 });
