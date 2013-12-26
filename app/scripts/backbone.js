c791650f9a92649a87dd94ddad0c2f3c2645cd1f
akh2103 <akh2103@gmail.com>
2013-12-26 15:01:16 -0600

    messages are knocked off

---
+++ app/scripts/backbone.js
@@ -0,0 +1,35 @@
+var EquipmentSubtotal = Backbone.Model.extend({
+  validate: function(attrs, options) {
+    if (attrs.quantity){
+    	 if (isNaN(attrs.quantity)){
+      		return "Quantity can't be less than 0";
+    	}
+    	if (attrs.quantity < 0) {
+      		return "Quantity can't be less than 0";
+    	}
+	}
+	else{
+		return "You must enter a quantity";
+	}
+	if (attrs.rate){
+    	 if (isNaN(attrs.rate)){
+      		return "Rate can't be less than 0";
+    	}
+    	if (attrs.rate < 0) {
+      		return "Rate can't be less than 0";
+    	}
+	}
+	else{
+		return "You must enter a rate";
+	}
+  }
+});
+
+var one = new EquipmentSubtotal({
+  title : "Chapter One: The Beginning"
+});
+
+
+one.on("invalid", function(model, error) {
+  	alert(model.get(error));
+});
---
+++ app/scripts/backbone_components.js
@@ -0,0 +1,15 @@
+var Chapter = Backbone.Model.extend({
+  validate: function(attrs, options) {
+    if (attrs.end < attrs.start) {
+      return "can't end before it starts";
+    }
+  }
+});
+
+var one = new Chapter({
+  title : "Chapter One: The Beginning"
+});
+
+one.on("invalid", function(model, error) {
+  alert(model.get("title") + " " + error);
+});
